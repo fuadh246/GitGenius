39b689abbd008124b0d6feb732c69b629e15fd47
Ashwin835 <ashwin75335@gmail.com>
2024-06-24 20:11:08 -0400

    Added RAG base code, just need vector db credentails and openai credentials

---
+++ RAG.py
@@ -0,0 +1,35 @@
+import openai
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+
+
+
+openai.api_key = api_key    #input your api key here
+
+prompt= f'''You are given project code files.
+
+Given the query specified here: {query},
+
+
+Analyze the code files and output the response
+'''
+
+
+query=""
+
+embeddings= OpenAIEmbeddings()
+
+db= FAISS(embeddings, docstore) #input vector database credentials here
+
+retriever = db.as_retriever(search_kwargs={'k': 4})
+
+docs= retriever.get_relevant_documents(query)
+
+llm= ChatOpenAI()
+
+
+chain= load_qa_with_sources_chain(llm=llm, chain_type="stuff")
